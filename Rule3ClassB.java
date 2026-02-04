24aaba2c661df24cca542820c0279bbbb099c5c5
tuanznguyen <redmi15052003@gmail.com>
2026-02-04 22:07:49 +0700

    Add files via upload

---
+++ Rule3ClassB.java
@@ -0,0 +1,11 @@
+
+package lec11Rule3;
+
+public class Rule3ClassB {
+    public void display() {
+        Rule3ClassA obj = new Rule3ClassA(); 
+        System.out.println(obj.df);
+        System.out.println(obj.pro);
+        System.out.println(obj.pbl);
+    }
+}
